02d0f9f5cf60fe87cef82b80c26f7213168d0b70
Ireneusz Trepka <irek.trepka@gmail.com>
2020-01-05 17:53:10 +0100

    added bootstrap, simple layout template and homepage

---
+++ src/main/java/pl/pretkejshop/webstore/view/MainViewController.java
@@ -0,0 +1,12 @@
+package pl.pretkejshop.webstore.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainViewController {
+    @GetMapping({"", "/", "/index", "/index.html"})
+    public String displayHomepage() {
+        return "homepage";
+    }
+}
