ec9943ab147f2d7912d78d94ac66c2b8fb84ea73
Ireneusz Trepka <irek.trepka@gmail.com>
2020-01-11 21:22:58 +0100

    added spring boot security, customized own login form with config

---
+++ src/main/java/pl/pretkejshop/webstore/config/AppConfig.java
@@ -0,0 +1,48 @@
+package pl.pretkejshop.webstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable() // tymczasowe wylaczenie zabezpieczenia przed csrf
+                .authorizeRequests() //autoryzuj wszystkie zadania
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin() //generowanie formularza logowania
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .permitAll()
+                .and()
+                .logout();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/pl/pretkejshop/webstore/view/UserViewController.java
+++ src/main/java/pl/pretkejshop/webstore/view/UserViewController.java
@@ -1,8 +1,27 @@
 package pl.pretkejshop.webstore.view;
 
+import io.swagger.models.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+import pl.pretkejshop.webstore.model.User;
+import pl.pretkejshop.webstore.repository.UserRepository;
+import pl.pretkejshop.webstore.service.dto.UserDto;
+import pl.pretkejshop.webstore.service.exception.NotFoundException;
+import pl.pretkejshop.webstore.service.services.UserService;
 
 @Controller
 public class UserViewController {
+    @Autowired
+    private UserService userService;
 
+    @GetMapping("/user/{id}")
+    public ModelAndView displayMyAccountPage(@PathVariable Integer id) throws NotFoundException {
+        UserDto user = userService.getUserById(id);
+        ModelAndView mv = new ModelAndView("my-account");
+        mv.addObject("user", user);
+        return mv;
+    }
 }
