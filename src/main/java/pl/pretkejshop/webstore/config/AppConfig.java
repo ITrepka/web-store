b4cb1deebcc12855683fa106c480b3a50f97c1ac
Ireneusz Trepka <irek.trepka@gmail.com>
2020-01-12 16:35:59 +0100

    header and footer as layout, refactored html's

--- src/main/java/pl/pretkejshop/webstore/config/AppConfig.java
+++ src/main/java/pl/pretkejshop/webstore/config/AppConfig.java
@@ -1,11 +1,7 @@
 package pl.pretkejshop.webstore.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +9,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebSecurity
