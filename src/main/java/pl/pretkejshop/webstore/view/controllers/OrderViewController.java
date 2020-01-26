ccf8abdafcddfbc492c2c2ad8e485a8bef49bcf7
Ireneusz Trepka <irek.trepka@gmail.com>
2020-01-26 23:14:56 +0100

    initial logic of data transfer between view and controller handling submit order

--- src/main/java/pl/pretkejshop/webstore/view/controllers/OrderViewController.java
+++ src/main/java/pl/pretkejshop/webstore/view/controllers/OrderViewController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import pl.pretkejshop.webstore.model.Order;
+import pl.pretkejshop.webstore.service.dto.CreateUpdateShippingDetailsDto;
 import pl.pretkejshop.webstore.service.dto.DeliveryTypeDto;
 import pl.pretkejshop.webstore.view.service.services.CartViewService;
 import pl.pretkejshop.webstore.view.service.services.OrderViewService;
@@ -26,7 +28,16 @@ public class OrderViewController {
         ModelAndView mv = new ModelAndView("submit-your-order");
         mv = cartViewService.getCurrentCart(session, mv);
         List<DeliveryTypeDto> deliveryTypesList = orderViewService.getDeliveryTypes();
-        mv.addObject("deliveryTypesList");
+        mv.addObject("deliveryTypesList", deliveryTypesList);
+        mv.addObject("shippingDetails", new CreateUpdateShippingDetailsDto());
+        return mv;
+    }
+
+    @PostMapping("/submit-your-order")
+    public ModelAndView submitOrder(@ModelAttribute CreateUpdateShippingDetailsDto shippingDetailsDto,
+                                    @RequestParam Long deliveryTypeId, @RequestParam Integer paymentTypeId) {
+        //todo adding order
+        ModelAndView mv = new ModelAndView("order-info");
         return mv;
     }
 
