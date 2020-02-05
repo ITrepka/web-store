fb11c504abe645b80be0a54a467c8df4543582bb
Ireneusz Trepka <irek.trepka@gmail.com>
2020-02-05 15:06:18 +0100

    homepage view edited, added specified group of products from database

--- src/main/java/pl/pretkejshop/webstore/view/controllers/MainViewController.java
+++ src/main/java/pl/pretkejshop/webstore/view/controllers/MainViewController.java
@@ -1,12 +1,31 @@
 package pl.pretkejshop.webstore.view.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.pretkejshop.webstore.service.exception.NotFoundException;
+import pl.pretkejshop.webstore.view.service.dto.ProductViewDto;
+import pl.pretkejshop.webstore.view.service.services.ShopViewService;
+
+import java.util.List;
 
 @Controller
 public class MainViewController {
+    @Autowired
+    private ShopViewService shopViewService;
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String displayHomepage() {
-        return "homepage";
+    public ModelAndView displayHomepage() throws NotFoundException {
+        List<ProductViewDto> allAvaibleProducts = shopViewService.getAllAvaibleProducts();
+        List<ProductViewDto> shirts = shopViewService.searchProductByText("shirt", allAvaibleProducts);
+        List<ProductViewDto> shoes = shopViewService.searchProductByText("shoes", allAvaibleProducts);
+        List<ProductViewDto> jeans = shopViewService.searchProductByText("jeans", allAvaibleProducts);
+        ModelAndView mv = new ModelAndView("homepage");
+        mv.addObject("shirts", shirts);
+        mv.addObject("shoes", shoes);
+        mv.addObject("jeans", jeans);
+
+        return mv;
     }
 }
